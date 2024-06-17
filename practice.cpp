85c82a055189a3a6dbaea7b28e8e1d5487de6cb7
Himanshu raj <110243579+coder23-himasnhu@users.noreply.github.com>
2024-06-17 20:13:28 +0530

    practice
    practice some algorithms of DSA

--- practice.cpp
+++ practice.cpp
@@ -1,11 +1,20 @@
-// copy constructor
 
 #include<iostream>
 using namespace std;
 
-class Student
+int main()
 {
-    public:
-    string name;
-    int roll;
-};
+    int arr[100]={6,4,5,-3,2,8};
+    
+    int prefix[100];
+    prefix[0]= arr[0];
+
+    for(int i=1; i<6; i++)
+    {
+      prefix[i] = prefix[i-1]+arr[i];
+    }
+    for(int i=0; i<6; i++)
+    {
+      cout<<prefix[i]<<" ";
+    }
+}
