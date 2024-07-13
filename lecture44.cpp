736dee8056f344a631b96332fdeba5394595c132
Himanshu raj <110243579+coder23-himasnhu@users.noreply.github.com>
2024-07-13 22:19:12 +0530

    Linked List
    Started Linked List

---
+++ lecture44.cpp
@@ -0,0 +1,6 @@
+// Linked List:-
+// The data present in the linked list is in distinct memory location whereas the data present in the array is in continuos manner
+// Any address in the linked list has data as well as address of next data both. So, we can easily find the next address of data
+// New keyword dynically way me node create karta hai
+
+// 17 Minute pe stop kiya hai linked list ke video ko coder army
