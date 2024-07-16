fcf2922b55a7e92fb3a2e682693731dd39f17eca
Himanshu raj <110243579+coder23-himasnhu@users.noreply.github.com>
2024-07-16 22:21:51 +0530

    OOPS
    practice some concept of OOPS

--- practice2.cpp
+++ practice2.cpp
@@ -1,61 +1,22 @@
-// #include<iostream>
-// using namespace std;
-
-// class Human
-// {
-//     protected:
-//     string name;
-//     int age;
-
-//     public:
-//     void work()
-//     {
-//         cout<<"I am WOrking\n" ;
-//     }
-// };
-// class Student: public Human
-// {
-//     int roll_number, fees;
-
-//     public:
-//     Student(string name, int age, int roll_num, int fees)
-//     {
-//         this->name = name;
-//         this->age = age;
-//         this->roll_number = roll_num;
-//         this->fees = fees;
-//     }
-// };
-// int main()
-// {
-//     Student A1("Rohit", 26, 32, 99);
-//     A1.work();
-// }
-
-
-
 #include<iostream>
 using namespace std;
 
-class Human
+class Circle
 {
-    string name;
-    int age;
-}
-class Student:public Human
-{
-    int roll;
-
-    Student(string name, int age, int roll)
+    public:
+    int area(int r)
     {
-        this->name = name;
-        this->age = age;
-        this->roll = roll;
+        return 2*3.14*r;
     }
-}
+    int area(int l, int b)
+    {
+        return l*b;
+    }
+};
 
 int main()
 {
-    Student S1;
-    S1.
+    Circle C1;
+    cout<<C1.area(2)<<endl;
+    cout<<C1.area(2,3);
 }
