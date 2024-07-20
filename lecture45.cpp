d4c833b1291e380733c1956f9c9ae8720327552e
Himanshu raj <110243579+coder23-himasnhu@users.noreply.github.com>
2024-07-20 17:32:48 +0530

    Linked List
    Practice some Linked List Questions on GFG

--- lecture45.cpp
+++ lecture45.cpp
@@ -63,73 +63,77 @@
 // 3)linked list ek node jyda hai
 // if we handle all three cases then our program is right
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-class Node
-{
-    public:
-    int data;
-    Node *next;
+// class Node
+// {
+//     public:
+//     int data;
+//     Node *next;
 
-    Node(int value)
-    {
-        data = value;
-        next = NULL;
-    }
-};
-    // function to create linked list
- Node *CreateLinkedList(int arr[], int index, int size)
-    {
-        if(index==size)
-        return NULL;
-        // int arr[] = {2,4,6,1,7};
+//     Node(int value)
+//     {
+//         data = value;
+//         next = NULL;
+//     }
+// };
+//     // function to create linked list
+//  Node *CreateLinkedList(int arr[], int index, int size)
+//     {
+//         if(index==size)
+//         return NULL;
+//         // int arr[] = {2,4,6,1,7};
 
-        Node *temp = new Node(arr[index]);
-        temp->next = CreateLinkedList(arr,index+1, size);
-        return temp;
-    }
+//         Node *temp = new Node(arr[index]);
+//         temp->next = CreateLinkedList(arr,index+1, size);
+//         return temp;
+//     }
 
-int main()
-{
-    Node *Head;
-    Head = NULL;  //tail aur head dono me NULL hai
-    int arr[] = {2,4,6,8,10};
+// int main()
+// {
+//     Node *Head;
+//     Head = NULL;  //tail aur head dono me NULL hai
+//     int arr[] = {2,4,6,8,10};
     
-    Head = CreateLinkedList(arr, 0, 5);
+//     Head = CreateLinkedList(arr, 0, 5);
 
-    //delete a node at end
-    // check if node exist
-    if(Head!=NULL)
-    {
-        //if only node is present
-        if(Head->next == NULL)
-        {
-            // Node *temp = Head;
-            delete Head;
-            Head = NULL;
-        }
-        // if More thn an one node is present(in this case we have to take two pointer which point on last and secenond last node)
-        else{
-            Node *curr = Head;
-            Node *prev = NULL;
-            while(curr->next!=NULL)
-            {
-                prev = curr;
-                curr = curr->next;
-            }
-            prev->next = NULL;
-            delete curr;
-        }
-    }
-    // printing the node
-        Node *temp = Head;
-    while (temp != NULL)
-    {
-        cout << temp->data << " ";
-        temp = temp->next;
-    }
+//     //delete a node at end
+//     // check if node exist
+//     if(Head!=NULL)
+//     {
+//         //if only node is present
+//         if(Head->next == NULL)
+//         {
+//             // Node *temp = Head;
+//             delete Head;
+//             Head = NULL;
+//         }
+//         // if More thn an one node is present(in this case we have to take two pointer which point on last and secenond last node)
+//         else{
+//             Node *curr = Head;
+//             Node *prev = NULL;
+//             while(curr->next!=NULL)
+//             {
+//                 prev = curr;
+//                 curr = curr->next;
+//             }
+//             prev->next = NULL;
+//             delete curr;
+//         }
+//     }
+//     // printing the node
+//         Node *temp = Head;
+//     while (temp != NULL)
+//     {
+//         cout << temp->data << " ";
+//         temp = temp->next;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
+
+
+// Delete a Particular Node
+// GFG 
