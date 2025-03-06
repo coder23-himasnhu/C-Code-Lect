f8d9d26e0eaedb5619cc6291a041fbd6502ee00e
Himanshu raj <110243579+coder23-himasnhu@users.noreply.github.com>
2025-03-06 17:21:00 +0530

    Dequeue
    Implement Dequeue using Array

---
+++ lecture54.cpp
@@ -0,0 +1,156 @@
+// Implement dequeue with Array:-
+// to implement dequeue with array we have to treat the array as circular array
+
+#include <iostream>
+using namespace std;
+
+class dequeue
+{
+    int front, rear, size;
+    int *arr;
+
+    public:
+    dequeue(int n)
+    {
+        size  = n;
+        arr = new int[n];
+        front = rear = -1;
+    }
+
+    bool IsEmpty()
+    {
+        return front == -1;
+    }
+
+    bool IsFull()
+    {
+        return (rear+1)%size+front;
+    }
+
+    void push_front(int x)
+    {
+        //Empty
+        if(IsEmpty())
+        {
+            front=rear=0;
+            cout<<"Pushed "<<x<<"in front\n";
+            arr[0]=x;
+            return;
+        }
+        //Full
+        else if(IsFull())
+        {
+            cout<<"Dequue Overflow\n";
+            return;
+        }
+        // Normal
+        else{
+            front = (front-1+size)%size;
+            arr[front] = x;
+            cout<<"Pushed "<<x<<"in front\n";
+            return;
+        }
+    }
+
+    void push_back(int x)
+    {
+        //Empty
+        if(IsEmpty())
+        {
+            front=rear=0;
+            cout<<"Pushed "<<x<<"in back\n";
+            arr[0]=x;
+            return;
+        }
+        //Full
+        else if(IsFull())
+        {
+            cout<<"Dequue Overflow\n";
+            return;
+        }
+        // Normal
+        else{
+            rear = (rear+1)%size;
+            arr[rear] = x;
+            cout<<"Pushed "<<x<<"in back\n";
+            return;
+        }
+    }
+
+    void pop_front()
+    {
+        //Empty
+        if(IsEmpty())
+        {
+            cout<<"Dequqe Underflow\n";
+            return;
+        }
+        else{
+            cout<<"Popped "<<arr[front]<<"from dequeue";
+            //single element
+            if(front==rear)
+            {
+                front = rear = -1;
+            }
+            //grater than 1 element
+            else{
+                front = (front+1)%size;
+            }
+        }
+    }
+
+    void pop_back()
+    {
+        //Empty
+        if(IsEmpty())
+        {
+            cout<<"Dequqe Underflow\n";
+            return;
+        }
+        else{
+            cout<<"Popped "<<arr[rear]<<"from dequeue";
+            //single element
+            if(front==rear)
+            {
+                front = rear = -1;
+            }
+            //grater than 1 element
+            else{
+                rear = (rear-1+size)%size;
+            }
+        }
+    }
+
+    int start()
+    {
+        if(IsEmpty())
+        {
+            return -1;
+        }
+        else{
+            return arr[front];
+        }
+    }
+
+    int end()
+    {
+        if(IsEmpty())
+        {
+            return -1;
+        }
+        else{
+            return arr[rear];
+        }
+    }
+};
+
+int main()
+{
+    dequeue d(5);
+    d.push_back(10);
+    d.push_back(9);
+    d.push_front(13);
+    d.push_back(19);
+    d.push_front(13);
+    cout<<d.start()<<endl;
+}
