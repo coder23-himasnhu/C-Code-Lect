31ccfb0dffa94700ddac7c5e9ba2deaf2f657a1f
Himanshu raj <110243579+coder23-himasnhu@users.noreply.github.com>
2025-03-23 11:47:54 +0530

    Binary Tree
    Implement Binary Tree in DFS tectnique

--- lecture55.cpp
+++ lecture55.cpp
@@ -3,8 +3,60 @@
 // If n node are present in binary tree then there are total n-1 edge
 
 // Implement Tree:- 
+// #include <iostream>
+// #include <queue>
+// using namespace std;
+
+// class Node
+// {
+//     public:
+//     int data;
+//     Node *left, *right;
+
+//     Node(int value)
+//     {
+//         data = value;
+//         left = right = NULL;
+//     }
+// };
+
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the Root Element:- ";
+//     cin>>x;
+//     int first, seceond;
+//     queue<Node*>q;
+//     Node *root = new Node(x);
+//     q.push(root);
+
+//     //Build the Binary Tree
+//     while(!q.empty())
+//     {
+//         Node *temp = q.front();
+//         q.pop();
+//         cout<<"Enter the Left child of "<<temp->data<<":";
+//         cin>>first; //Left Node ki value
+//         //left Node
+//         if(first!=-1)
+//         {
+//             temp->left = new Node(first);
+//             q.push(temp->left);
+//         }
+//         //right Node
+//         cout<<"Enter the Left child of "<<temp->data<<":";
+//         cin>>seceond;
+//         if(seceond!=-1)
+//         {
+//             temp->right = new Node(seceond);
+//             q.push(temp->right);
+//         }
+//     }
+// }
+
+
+// Create the tree with another method:-
 #include <iostream>
-#include <queue>
 using namespace std;
 
 class Node
@@ -20,36 +72,28 @@ class Node
     }
 };
 
-int main()
+Node *BinaryTree()
 {
     int x;
-    cout<<"Enter the Root Element:- ";
     cin>>x;
-    int first, seceond;
-    queue<Node*>q;
-    Node *root = new Node(x);
-    q.push(root);
-
-    //Build the Binary Tree
-    while(!q.empty())
+    if(x== -1)
     {
-        Node *temp = q.front();
-        q.pop();
-        cout<<"Enter the Left child of "<<temp->data<<":";
-        cin>>first; //Left Node ki value
-        //left Node
-        if(first!=-1)
-        {
-            temp->left = new Node(first);
-            q.push(temp->left);
-        }
-        //right Node
-        cout<<"Enter the Left child of "<<temp->data<<":";
-        cin>>seceond;
-        if(seceond!=-1)
-        {
-            temp->right = new Node(seceond);
-            q.push(temp->right);
-        }
+        return NULL;
     }
+    Node *temp = new Node(x);
+    //Left Side create 
+    cout<<"Enter the Left child of "<<x<<" ";
+    temp->left = BinaryTree();  //yaha agar value +ve hai, to right child bhi create ho jata hai par value nhi ja sakta hai bcz. wo phir se usi function ko call kar raha hai
+    //right side create
+    cout<<"Enter the Right child of "<<x<<" ";
+    temp->right = BinaryTree();
+    return temp;
+}
+
+int main()
+{
+    cout<<"Enter the Root Node:- ";
+    Node *root;
+    root = BinaryTree();
+
 }
